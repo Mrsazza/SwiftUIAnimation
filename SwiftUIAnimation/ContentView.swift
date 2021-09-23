fc9a9071dd9aca7bebc2d6fdaefc0537deb900b0
Mrsazza <shahriarhossain147@gmail.com>
2021-09-24 02:08:38 +0600

    Initial Commit

---
+++ SwiftUIAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimation
+//
+//  Created by Sazza on 24/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
